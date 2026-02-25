498bff651c39e123b6f28a260f5de571b5a077b3
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2026-02-25 12:22:18 -0500

    Centered entire app to middle of screen and styled the header

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ function App() {
   
   return (
     <>
-      <div>
-         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flame-icon lucide-flame"><path d="M12 3q1 4 4 6.5t3 5.5a1 1 0 0 1-14 0 5 5 0 0 1 1-3 1 1 0 0 0 5 0c0-2-1.5-3-1.5-5q0-2 2.5-4"/></svg>
-          <h1>Cal Track</h1>
+      <div className="header">
+
+        <div className="logo">
+         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flame-icon lucide-flame flame-icon-style"><path d="M12 3q1 4 4 6.5t3 5.5a1 1 0 0 1-14 0 5 5 0 0 1 1-3 1 1 0 0 0 5 0c0-2-1.5-3-1.5-5q0-2 2.5-4"/></svg>
+          <h1 className="logo-name">CalTrack</h1>
+        </div>
+        <p className="title-subheading">Set your daily goal, log your meals, stay on track.</p>
+
       </div>
-      <p>Set your daily goal, log your meals, stay on track.</p>
+      
+
       <div>
         <div className="card-one">
 
