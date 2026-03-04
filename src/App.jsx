6f08ab1b388857f01fcf716761004cbd5add1e69
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2026-03-04 08:51:36 -0500

    Styled the goal input and added width to cards container

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,11 @@ function App() {
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-goal-icon lucide-goal small-icon-style"><path d="M12 13V2l8 4-8 4"/><path d="M20.561 10.222a9 9 0 1 1-12.55-5.29"/><path d="M8.002 9.997a5 5 0 1 0 8.9 2.02"/></svg>
               <h3>Daily Calorie Goal</h3>
             </div>
+            <div className="goal-form">
+              <input type="number" placeholder="Current: 2000 cal" className="goal-input-style" id="goal-input"/>
+              <button>Update Goal</button>
+            </div>
             
-            <input type="text" placeholder="Current: 2000cal"/>
-            <button>Update Goal</button>
           </div>
 
         </div>
