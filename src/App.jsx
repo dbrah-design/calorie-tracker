9b61b4374e30249399fa5b64fc580f85457b4146
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2026-03-05 12:20:45 -0500

    Styled default message and added padding to todays log and log food container

--- src/App.jsx
+++ src/App.jsx
@@ -60,12 +60,14 @@ function App() {
         <div className="card-two">
 
           <div className="todays-log">
+
             <div className='icon-flex'>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flame-icon lucide-flame small-icon-style"><path d="M12 3q1 4 4 6.5t3 5.5a1 1 0 0 1-14 0 5 5 0 0 1 1-3 1 1 0 0 0 5 0c0-2-1.5-3-1.5-5q0-2 2.5-4"/></svg>
-              <h3>Today's log</h3>
+              <h3>Today's Log</h3>
             </div>
             
-            <p>No food logged yet. Start adding meals below</p>
+            <p className="default-message">No food logged yet. Start adding meals below</p>
+
           </div>
 
            <div className="border" ></div>
