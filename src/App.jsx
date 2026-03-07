9a63f53509d0b3b4a2f1a3a37d6b9ec3670402fb
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2026-03-07 09:12:39 -0500

    Styled log food inputs and add food button

--- src/App.jsx
+++ src/App.jsx
@@ -79,9 +79,15 @@ function App() {
               <h3>Log Food</h3>
             </div>
 
-            <input type="text" placeholder="Food name" />
-            <input type="text" placeholder="Calories" />
-            <button><span><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg></span> Add Food</button>
+            <div className="log-food-form">
+              
+              <input type="text" placeholder="Food name" className="food-name-input"/>
+              <input type="number" placeholder="Calories" className="food-cal-input"/>
+              <button className="add-food"><span><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus plus-icon"><path d="M5 12h14"/><path d="M12 5v14"/></svg></span> Add Food</button>
+
+            </div>
+
+            
             
            </div>
 
