f080f663eb6d46e21eb37b093e2c4e08f636e795
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2026-03-08 09:04:59 -0400

    Removed span and justify content to fix add food button alignment issue

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,11 @@ function App() {
               
               <input type="text" placeholder="Food name" className="food-name-input"/>
               <input type="number" placeholder="Calories" className="food-cal-input"/>
-              <button className="add-food"><span><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus plus-icon"><path d="M5 12h14"/><path d="M12 5v14"/></svg></span> Add Food</button>
+              <button className="add-food">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus plus-icon"><path d="M5 12h14"/><path d="M12 5v14"/>
+                </svg>
+                Add Food
+              </button>
 
             </div>
 
